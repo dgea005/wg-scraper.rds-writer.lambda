b7e7d8ae14441ff7f763d1b042249008a838897a
dgea005 <danielgeals@gmail.com>
2017-11-25 01:06:10 +0100

    experiment

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,10 +6,21 @@ import asyncpg
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+DB_USER = os.environ['db_user']
+DB_PASS = os.environ['db_pass']
+DB_ENDPOINT = os.environ['db_endpoint']
+
 def lambda_handler(event, context):
-    db_user = os.environ['db_user']
-    db_pass = os.environ['db_pass']
-    db_endpoint = os.environ['db_endpoint']
-    logger.info(db_user)
-    logger.info(db_endpoint)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
     return
+
+
+async def run():
+    conn = await asyncpg.connect(user=DB_USER, password=DB_PASS, database='ScraperDatabase', host=DB_ENDPOINT)
+    values = await conn.fetch('''SELECT * FROM index_raw''')
+    logger.info(values)
+    await conn.close()
+
+
