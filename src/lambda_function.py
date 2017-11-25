abeea381aff80dc6ee8793fa69722310d9b8a5ea
dgea005 <danielgeals@gmail.com>
2017-11-25 23:11:52 +0100

    read from template and print

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import base64
 import asyncio
 import asyncpg
 
@@ -11,19 +12,23 @@ DB_PASS = os.environ['db_pass']
 DB_ENDPOINT = os.environ['db_endpoint']
 
 def lambda_handler(event, context):
+    for record in event['Records']:
+       #Kinesis data is base64 encoded so decode here
+       payload=base64.b64decode(record["kinesis"]["data"])
+       print("Decoded payload: " + str(payload))
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
-    return
+# def lambda_handler(event, context):
 
-query = """
-INSERT INTO index_raw (listing_id, link, cost, size, stadt, free_from, free_to, stay_length, scrape_time, flat_type)
-  VALUES  (630515, 'http://some_link', 400, 18, 'Mitte', '2017-08-27', '2017-09-23', 27, '2017-08-24 07:20:13', 'studio');
-"""
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(run())
+#     return
 
-async def run():
-    conn = await asyncpg.connect(user=DB_USER, password=DB_PASS, database='ScraperDatabase', host=DB_ENDPOINT)
-    await conn.execute(query)
-    await conn.close()
+# query = """
+# INSERT INTO index_raw (listing_id, link, cost, size, stadt, free_from, free_to, stay_length, scrape_time, flat_type)
+#   VALUES  (630515, 'http://some_link', 400, 18, 'Mitte', '2017-08-27', '2017-09-23', 27, '2017-08-24 07:20:13', 'studio');
+# """
 
-
+# async def run():
+#     conn = await asyncpg.connect(user=DB_USER, password=DB_PASS, database='ScraperDatabase', host=DB_ENDPOINT)
+#     await conn.execute(query)
+#     await conn.close()
