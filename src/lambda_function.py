880b77487d9d4079856550e90348a5e297153b43
dgea005 <danielgeals@gmail.com>
2017-11-26 13:29:56 +0100

    first attempt loading json data from kinesis

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,6 +3,8 @@ import os
 import base64
 import asyncio
 import asyncpg
+import json
+from io import StringIO
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -11,11 +13,28 @@ DB_USER = os.environ['db_user']
 DB_PASS = os.environ['db_pass']
 DB_ENDPOINT = os.environ['db_endpoint']
 
+# schema will be this
+# but should add event_name
+# meta_created_at
+# event version
+    # listing_id INTEGER DEFAULT NULL,
+    # link TEXT DEFAULT NULL,
+    # cost SMALLINT DEFAULT NULL,
+    # size SMALLINT DEFAULT NULL,
+    # stadt TEXT DEFAULT NULL,
+    # free_from DATE DEFAULT NULL,
+    # free_to DATE DEFAULT NULL,
+    # stay_length SMALLINT DEFAULT NULL,
+    # scrape_time TIMESTAMP DEFAULT NULL,
+    # flat_type VARCHAR(50) DEFAULT '---'
+
 def lambda_handler(event, context):
     for record in event['Records']:
        #Kinesis data is base64 encoded so decode here
        payload=base64.b64decode(record["kinesis"]["data"])
-       print("Decoded payload: " + str(payload))
+       #print("Decoded payload: " + str(payload))
+       data = json.loads(StringIO(str(payload)))
+       print("Loaded payload: " + data)
 
 # def lambda_handler(event, context):
 
