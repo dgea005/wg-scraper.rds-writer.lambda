ce0b5c19e02f67d25b4a25d467e42c8c4183a144
dgea005 <danielgeals@gmail.com>
2017-11-26 15:07:52 +0100

    transformation function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,6 +4,7 @@ import base64
 import asyncio
 import asyncpg
 import json
+from dateutil.parser import parse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -33,7 +34,61 @@ def lambda_handler(event, context):
        payload=base64.b64decode(record["kinesis"]["data"])
        #print("Decoded payload: " + str(payload))
        data = json.loads(payload)
-       print("Loaded payload: " + str(data))
+       transformed = transform_wg_data(data)
+       print("Loaded payload: " + str(transformed))
+
+
+def validate_event(data):
+    """
+    when event has event_name and meta_created_at added
+    then we want to make sure these fields exist and are valid
+    that is, that they contain the data required
+
+    returns if event is valid and transformation function to call (depending on name space)
+    could also return the event name for logging and which function it will call
+    """
+    ## check what the keys are
+    ## 
+
+    pass
+
+
+def transform_wg_data(data):
+    ## could have different event names
+    ## depending on the event name then call different versions of this function
+    ## should this also validate the data?
+    """
+    for wg index data v0
+    if event is valid and event name is index_v0
+    then transform to python data types with this function
+
+    returns: transform data dictionary
+
+    should wrap call in try except to say if data could be transformed
+    """
+    # some of this is probably not necessary
+    transformed_data = dict()
+    transformed_data['listing_id'] = int(data['listing_id'])
+    transformed_data['link'] = str(data['link'])
+    transformed_data['cost'] = int(data['cost'])
+    transformed_data['size'] = int(data['size'])
+    transformed_data['stadt'] = str(data['stadt'])
+    ## will need empty error handling here
+    transformed_data['free_from'] = parse(data['free_from'].date())
+    transformed_data['free_to'] = parse(data['free_to'].date())
+    transformed_data['stay_length'] = int(data['stay_length'])
+    transformed_data['scrape_time'] = parse(data['scrape_time'])
+    transformed_data['flat_type'] = str(data['flat_type'])
+    return transformed_data
+
+def transform_to_postgres_input(transformed_dict):
+    """
+    transform validated data into data that can be inserted to database
+
+    returns: query string to be inserted
+    """
+    pass
+
        
 
 # def lambda_handler(event, context):
